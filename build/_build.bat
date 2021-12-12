511cefe12e1c91fb1d9d7c55cbb4ae9272c0ae48
Tengu712 <genreihoutengu@gmail.com>
2021-12-12 19:02:10 +0900

    readme and bat

--- build/_build.bat
+++ build/_build.bat
@@ -1,18 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set kCL="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30037\bin\Hostx86\x86\cl.exe"
 set kOptInclude=^
-/I "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30037\include" ^
-/I "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\VS\include" ^
+/I "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.30.30705\include" ^
+/I "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\VS\include" ^
 /I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt" ^
 /I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um" ^
 /I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\shared" ^
 /I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\winrt" ^
 /I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\cppwinrt"
 set kOptLibrary=/link ^
-/LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30037\lib\x86" ^
-/LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\VS\lib\x86" ^
+/LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.30.30705\lib\x86" ^
+/LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\VS\lib\x86" ^
 /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\ucrt\x86" ^
 /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\um\x86" ^
 /LIBPATH:"."
@@ -30,6 +29,6 @@ if "!pathFiles!"=="" (
     exit /B
 )
 
-!kCL! /EHsc /Fe:th_abp.exe !kOptInclude! !pathFiles! !kOptLibrary!
+cl /EHsc /Fe:th_abp.exe !kOptInclude! !pathFiles! !kOptLibrary!
 del *.obj
 pause
--- build/_directx.bat
+++ build/_directx.bat
@@ -1,11 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set kCL="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30037\bin\Hostx86\x86\cl.exe"
-set kLib="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30037\bin\Hostx86\x86\lib.exe"
 set kOptInclude=^
-/I "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30037\include" ^
-/I "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\VS\include" ^
+/I "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.30.30705\include" ^
+/I "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\VS\include" ^
 /I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt" ^
 /I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um" ^
 /I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\shared" ^
@@ -25,7 +23,7 @@ if "!pathFiles!"=="" (
     exit /B
 )
 
-!kCL! /c /EHsc !kOptInclude! !pathFiles!
+cl /c /EHsc !kOptInclude! !pathFiles!
 
 set pathFilesObj=
 if exist ".\*.obj" (
